00a699d9c5a146e222c918853510c036f5846614
aifedespaix <f@d.p>
2018-10-18 14:58:18 +0200

    pd

--- src/states/state.ts
+++
@@ -1,9 +0,0 @@
-"use strict";
-/** Imports */
-import App from "..";
-
-abstract class State extends Phaser.State {
-  game: App; // It needs if we add property to `App` class
-}
-
-export default State;
