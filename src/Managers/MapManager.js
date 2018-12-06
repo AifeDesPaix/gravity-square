d2c3dfb8ce85fdb4917ae12ba18891c47b11764d
fdp <clapette01@gmail.com>
2018-12-06 20:26:12 +0100

    Revert "récupération des maps sur le serv EZ" ok
    This reverts commit aafc4320064bc2e65cfbec5785bfd8d23b494912.

--- src/Managers/MapManager.js
+++ src/Managers/MapManager.js
@@ -1,17 +1,13 @@
-/* eslint-disable array-callback-return */
+import screen from '../config/constants';
 import _Manager from './_Manager';
 import DefaultBackgroudObject from '../Objects/Backgrounds/Default';
 import DefaultPlatformObject from '../Objects/Platforms/Default';
-import Webservice from '../webservice/webservice';
 
 export default class MapManager extends _Manager {
   constructor(scene) {
     super(scene);
     this.objets = [];
     this.loadObjects();
-    this.tuileSize = 32;
-    this.initialPosX = 25;
-    this.mapping = {};
   }
 
   loadObjects() {
@@ -28,61 +24,80 @@ export default class MapManager extends _Manager {
     this.colidable = this.scene.physics.add.staticGroup();
     this.scene.add.image(400, 400, this.background.name);
     this.createMap();
+    console.log(this.objets[3]);
   }
 
-  async createMap() {
-    // @todo garder pour créer les maps
-    // const test = {
-    //   1: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    //   2: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    //   3: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    //   4: [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    //   5: [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    //   6: [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    //   7: [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-    //   8: [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   9: [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   10: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   11: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   12: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   13: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   15: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   16: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    //   17: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-    //   18: [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    //   19: [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    //   20: [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    //   21: [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    //   22: [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    //   23: [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    //   24: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    //   25: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    // };
+  update() {
+    // this.gravityChange(Direction.LEFT);
+    // return;
+    // this.objets[0].x = 150;
+    // this.objets[0].y = 750;
 
-    // On récupère une map en base de données.
-    // Si c'est trop long ou que l'on travaille hors connexion, recupérer le code ci-dessus
-    const ws = new Webservice('test');
-    this.mapping = (await ws.get('5g1Ot2omIKX7M81epEj4')).mapping;
+  }
 
-    let y = 30;
-    let x = this.initialPosX;
+  createMap() {
+    const tuileSize = 32;
+    const max = screen.WIDTH / tuileSize;
 
-    Object.keys(this.mapping).map((index) => {
-      Object.keys(this.mapping[index]).map((ind) => {
-        if (this.mapping[index][ind] === 1) {
-          this.putMapCase(x, y);
-        }
-        x += this.tuileSize;
+    let x = max / 2;
+    let y = max / 2;
+    for (let turn = 1; turn < 9; turn += 1) {
+      this.putMapSquare({
+        from: x,
+        to: x + (turn * 3),
+      }, {
+        from: y,
+        to: y,
       });
-      y += this.tuileSize;
-      x = this.initialPosX;
-    });
+      x += turn * 3;
+      this.putMapSquare({
+        from: x,
+        to: x,
+      }, {
+        from: y,
+        to: y + (turn * 3),
+      });
+      y += turn * 3;
+
+      turn += 1;
+
+      this.putMapSquare({
+        from: x - (turn * 3),
+        to: x,
+      }, {
+        from: y,
+        to: y,
+      });
+      x -= turn * 3;
+      this.putMapSquare({
+        from: x,
+        to: x,
+      }, {
+        from: y - (turn * 3),
+        to: y,
+      });
+      y -= turn * 3;
+    }
+  }
+
+  putMapSquare(width, height) {
+    for (let x = width.from; x <= width.to; x += 1) {
+      for (let y = height.from; y <= height.to; y += 1) {
+        this.putMapCase(x, y);
+      }
+    }
   }
 
   putMapCase(x, y) {
+    const [xT, yT] = [Math.trunc(x), Math.trunc(y)];
+    const tuileSize = 32;
+    const max = screen.WIDTH / tuileSize;
+    if (xT > max || yT > max) {
+      throw Error('Position en dehors');
+    }
     this.objets.push(this.colidable.create(
-      Math.trunc(x),
-      Math.trunc(y),
+      (tuileSize / 2) + (tuileSize * xT),
+      (tuileSize / 2) + (tuileSize * (max - 1 - yT)),
       this.platform.name,
     ));
   }
